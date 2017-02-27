6c0a0c87a6704070adbebfbf91b88f552cd440a7
maan0209 <maan0209@student.miun.se>
2017-02-27 14:21:25 +0100

    Klass för Postgress skapad

---
+++ Golf2/Golf2/Postgress.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+using System.Data;
+
+namespace Golf2
+{
+    public class Postgress
+    {
+        private NpgsqlConnection conn;
+        private NpgsqlCommand cmd;
+        private NpgsqlDataReader dr;
+        private DataTable tabel;
+
+        //Metod för att ansluta till databasen
+        public Postgress()
+        {
+            conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432;Database=dsu_g2;User Id=dsu_g2;Password=golfbil;Database=dsu_g2;SslMode=Require");
+
+            try
+            {
+                conn.Open();
+            }
+            catch (NpgsqlException ex)
+            {
+                string message = ex.Message;
+            }
+
+            tabel = new DataTable();
+        }
+
+        //Metod för att avsluta anslutningen till databasen
+        public void Close()
+        {
+            conn.Close();
+        }
+
+        //Metod för sql fråga
+        public DataTable sqlquestion(string sql)
+        {
+            cmd = new NpgsqlCommand(sql, conn);
+            dr = cmd.ExecuteReader();
+            tabel.Load(dr);
+            conn.Close();
+            return tabel;
+        }
+
+    }
+}
