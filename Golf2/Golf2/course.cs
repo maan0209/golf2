f18cef1624bbe72b897a0239e208e349441d2d0a
nathalie <naol1501>
2017-02-28 14:59:12 +0100

    Skapat klasser för person och för course med tillhörande properties

---
+++ Golf2/Golf2/course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Golf2
+{
+   
+
+    public class course
+    {
+        public int CourseId { get; set; }
+        public string Coursename { get; set; }
+        public DateTime Startdate { get; set; }
+        public DateTime Enddate { get; set; }
+        public bool Closed { get; set; }
+
+    }
+
+}
