41aaf641b63e06f78e1bdc5a73adc61b350c0413
nathalie <naol1501>
2017-02-28 11:40:07 +0100

    skapat person klass

---
+++ Golf2/Golf2/person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+       
+	}
+
+    public string golfId { get; set; }
+    public string gender { get; set; }
+    public string firstName { get; set; }
+    public string surName { get; set; }
+    public string email { get; set; }
+    public string postalcode { get; set; }
+    public string city { get; set; }
+    public int hcp { get; set; }
+    public string adress { get; set; }
+
+}
