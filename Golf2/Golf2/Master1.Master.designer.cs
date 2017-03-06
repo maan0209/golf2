c9dfc8209f7679dc6991ed78e818a61672896621
maan0209 <maan0209@student.miun.se>
2017-03-06 19:01:09 +0100

    Logga in funktion justerad.

--- Golf2/Golf2/Master1.Master.designer.cs
+++ Golf2/Golf2/Master1.Master.designer.cs
@@ -64,7 +64,7 @@ namespace Golf2 {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Log_In;
+        protected global::System.Web.UI.WebControls.LinkButton Log_In;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
