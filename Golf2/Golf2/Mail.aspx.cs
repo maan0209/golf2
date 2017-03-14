080730b1b783fab27f67338725d4c976a8cffd37
nathalie <naol1501>
2017-03-14 13:17:13 +0100

    Mail metod

--- Golf2/Golf2/Mail.aspx.cs
+++ Golf2/Golf2/Mail.aspx.cs
@@ -17,14 +17,14 @@ namespace Golf2
 
         protected void sendBtn_Click(object sender, EventArgs e)
         {
-            try { 
-            
+            try {
+             
             MailMessage message = new MailMessage(toTbx.Text,fromTbx.Text,subjectTbx.Text,bodyTbx.Text);
             message.IsBodyHtml = true;
 
             SmtpClient client = new SmtpClient("smtp-mail.outlook.com", 587);
             client.EnableSsl = true;
-
+            
             //I paranteserna nedan fick jag hårdkoda in min egna mail och lösen då funkade det men tar bort det nu när jag checkar in =)
             client.Credentials = new System.Net.NetworkCredential();
             client.Send(message);
@@ -38,31 +38,29 @@ namespace Golf2
 
         }
 
-        public string MailMessage(DateTime date, DateTime time, string typeOfMail, List<string> mailAddresses)
+        public void MailMessage(DateTime date, DateTime time, string typeOfMail, List<string> mailAddresses)
         {
             string notification;
-          
+
             if (typeOfMail == "booking")
             {
-                subjectTbx.Text = "Booking";
-                notification = "Du är inbokad i en boll klockan " + time + " den " + date;
-                bodyTbx.Text = notification;
-                return notification;
+                foreach(string s in mailAddresses)
+                {
+                    subjectTbx.Text = "Booking";
+                    notification = "Du är inbokad i en boll klockan " + time + " den " + date;                 
+                    bodyTbx.Text = notification;                   
+                }    
             }
-
-            else if(typeOfMail== "cancellation")
+        
+            else if (typeOfMail == "cancellation")
             {
-                subjectTbx.Text = "Cancellation";
-                notification = "Din bokning för " + date + " klockan " + time + " har blivit avbokad";
-                bodyTbx.Text = notification;
-                return notification;
-            }
-            else
-            {
-                return "";
-
-            }
+                foreach (string s in mailAddresses)
+                {
+                    subjectTbx.Text = "Cancellation";
+                    notification = "Din bokning för " + date + " klockan " + time + " har blivit avbokad";
+                    bodyTbx.Text = notification;                    
+                }              
+            }          
         }
-
     }
 }
